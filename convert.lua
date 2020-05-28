197f4cd5b9f0e7e543f8e979ec4d07acbd6908e6
VictorVG <victorvg04@gmail.com>
2020-05-28 19:01:54 +0300

    Converter Moon script to Lua
    Updated. Plase needed Moon-files in to som dir whats convert.lua and use lua:@convert.lua for run. After end all source Moon-file be automatically deleted!

---
+++ convert.lua
@@ -0,0 +1,19 @@
+﻿-- shmuz, 28.05.2020, updated VictorVG, 28.05.2020
+-- copy to root temporary folder included Moonscript files and run
+-- all Moonscript files be deleted after convert!
+local to_lua = (require "moonscript.base").to_lua
+far.RecursiveSearch(far.GetCurrentDirectory(), "*.moon",
+  function(item, fullpath)
+    local fp = assert(io.open(fullpath))
+    local str = fp:read("*all")
+    fp:close()
+    local newpath = fullpath:sub(1,-5).."lua"
+    fp = assert(io.open(newpath,"w"))
+    str = assert(to_lua(str))
+    fp:write(str, "\n")
+    fp:close()
+  end,
+  "FRS_RECUR")
+far.RecursiveSearch(far.GetCurrentDirectory(), "*.moon",
+  function(name, path) win.DeleteFile(path) end,
+  "FRS_RECUR")
