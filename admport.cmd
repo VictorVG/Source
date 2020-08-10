b28d269a0d6e2553b05c96f55f740f92eff97a7e
VictorVG <victorvg04@gmail.com>
2020-08-11 00:13:25 +0300

    Avidemux portable maker v2.1

--- admport.cmd
+++ admport.cmd
@@ -2,7 +2,7 @@
 setlocal enableextensions
 rem %1 is installer file name
 rem %2 is target dir
-if "%2" == "" (set wd=%~dps0) else (set wd=%2)
+if "%2" == "" (set wd=%cd%) else (set wd=%2)
 cd /d %wd%
 if "%1" == "" (if exist avidemux_*.* (set nm=avidemux_*.*) else (set nm=%1))
 if exist  %nm% (7z x %nm% -y -xr!$PLUGINSDIR -x![NSIS].nsi -x!uninstall.exe -o%wd%\ADM > nul)
@@ -13,5 +13,14 @@ for %%A in (Avidemux_*_settings.zip) do (7z x -y %%A -oADM\settings -x!readme.tx
 if exist ADM\pluginSettings (rmdir /s/q ADM\pluginSettings > nul)
 )
 if exist ADM\readme.txt (del /f/q ADM\readme.txt > nul)
-start /dADM ADM\avidemux.exe --portable
+if not exist avidemuxp.cmd (
+echo @echo off > avidemuxp.cmd
+echo setlocal enableextensions >> avidemuxp.cmd
+echo cd /d %%cd%% >> avidemuxp.cmd
+echo if exist "ADM\avidemux.exe" ^(start /dADM ADM\avidemux --portable^) >> avidemuxp.cmd
+echo goto:eof >> avidemuxp.cmd
+call avidemuxp.cmd
+  ) else (
+  call avidemuxp.cmd
+  )
 exit
