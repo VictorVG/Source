1989a11131e3d0f948c1ef40cf4ffa5027573974
VictorVG <victorvg04@gmail.com>
2020-11-04 09:08:32 +0300

    Prevent out
    Prevent out on current panel if position is rootdir and press CtrlPgDwn

---
+++ Shell_CtrlPgDnRoot.lua
@@ -0,0 +1,9 @@
+﻿Macro{
+  id="696B1377-27A6-421B-98C4-258FB6847DBC";
+  area="Shell";
+  key="CtrlPgDn";
+  description="Prevent out";
+  priority=60;
+  condition=function() return APanel.Root end;
+  action=function() Panel.SetPosIdx(0,0) end;
+}
