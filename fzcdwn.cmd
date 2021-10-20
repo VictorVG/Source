ac75a2abf111301440261742a25e589ce1dba7e7
VictorVG <victorvg04@gmail.com>
2021-10-20 23:00:35 +0300

    FileZilla Client, Server and libfilezilla download use cUR 7.66 or newer

--- fzcdwn.cmd
+++ fzcdwn.cmd
@@ -3,6 +3,7 @@ setlocal enableextensions
 if "%1" == "--help" goto mhelp
 if "%1" == "-h" goto mhelp
 if "%1" == "l" goto lfz else goto fzc
+if "%1" == "s" goto sfz else goto fzc
 :fzc
 if not "%1" == "" (
 set ver=%1
@@ -21,14 +22,41 @@ for %%A in (
      _win64.zip
      _win64-setup.exe
      _x86_64-linux-gnu.tar.bz2
-     ) do curl.exe -C - -LRZSs --create-dirs --output FileZilla\Client\FileZilla_%ver%%%A https://download.filezilla-project.org/client/FileZilla_%ver%%%A
-if not "%~1" == "" (curl.exe -C - -LRZSs --create-dirs --output FileZilla\Client\FileZilla_%ver%.sha512 https://download.filezilla-project.org/client/FileZilla_%ver%.sha512)
+     ) do curl.exe -C - -LRZSs --create-dirs --output FileZilla\client\FileZilla_%ver%%%A https://download.filezilla-project.org/client/FileZilla_%ver%%%A
+if not "%~1" == "" (curl.exe -C - -LRZSs --create-dirs --output FileZilla\client\FileZilla_%ver%.sha512 https://download.filezilla-project.org/client/FileZilla_%ver%.sha512)
 :lfz
 if "%2" == "" goto:eof
 for %%A in (
       tar.bz2
       sha512
      ) do curl.exe -C - -LRZSs --create-dirs --output FileZilla\libfilezilla\libfilezilla-%2.%%A https://download.filezilla-project.org/libfilezilla/libfilezilla-%2.%%A
+if "%3" == "" (goto:eof) else (goto sfz1)
+exit
+:sfz
+if "%2" == "" goto:eof
+for %%A in (
+      .sha512
+      _src.tar.bz2
+     ) do curl.exe -C - -LRZSs --create-dirs --output FileZilla\server\FileZilla_Server_%2%%A https://download.filezilla-project.org/server/FileZilla_Server_%2%%A
+set vr=%2:~1,1%
+if "%vr%" == "0" (
+      curl.exe -C - -LRZSs --create-dirs --output FileZilla\server\FileZilla_Server_%2.exe https://download.filezilla-project.org/server/FileZilla_Server_%2.exe
+      ) else (
+      curl.exe -C - -LRZSs --create-dirs --output FileZilla\server\FileZilla_Server_%2_win64-setup.exe https://download.filezilla-project.org/server/FileZilla_Server_%2_win64-setup.exe
+      )
+exit
+:sfz1
+if "%3" == "" goto:eof
+for %%A in (
+      .sha512
+      _src.tar.bz2
+     ) do curl.exe -C - -LRZSs --create-dirs --output FileZilla\server\FileZilla_Server_%3%%A https://download.filezilla-project.org/server/FileZilla_Server_%3%%A
+set vr=%3:~1,1%
+if "%vr%" == "0" (
+      curl.exe -C - -LRZSs --create-dirs --output FileZilla\server\FileZilla_Server_%3.exe https://download.filezilla-project.org/server/FileZilla_Server_%3.exe
+      ) else (
+      curl.exe -C - -LRZSs --create-dirs --output FileZilla\server\FileZilla_Server_%3_win64-setup.exe https://download.filezilla-project.org/server/FileZilla_Server_%3_win64-setup.exe
+      )
 exit
 :mhelp
 echo.
@@ -38,11 +66,12 @@ echo       This script is download FileZilla FTP Client and libfilezilla use cUR
 echo.
 echo Usage:
 echo.
-echo       fzcdwn ^<version^> [^<libfilezilla_version^>] or fzcdwn l ^<libfilezilla_version^>
+echo       fzcdwn ^<version^> [^<libfilezilla_version^>] [^<server_version^>]
+echo       or fzcdwn l ^<libfilezilla_version^> or fzcdwn s ^<server_version^>
 echo.
 echo Examples:
 echo.
-echo       fzcdwn 3.43.0 0.30.0 or fzcdwn 3.43.0 or fzcdwn l 0.30.0
+echo       fzcdwn 3.43.0 0.30.0 or fzcdwn 3.43.0 or fzcdwn l 0.30.0 or fzcdwn s 1.0.0
 echo.
 pause
 exit /b
